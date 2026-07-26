6b2eeb7de26fa4e4d63022e0f75ebd7886ace91f
codewithtabish <kashisultan099@gmail.com>
2026-07-26 08:25:41 +0500

    added props

--- src/app/(pages)/blogs/[slug]/page.tsx
+++ src/app/(pages)/blogs/[slug]/page.tsx
@@ -19,16 +19,17 @@
 //   return <main>Blog works</main>;
 // }
 
-import React from 'react'
 
-const SingleBlogPage = () => {
-  return (
-    <div>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis accusamus praesentium repudiandae esse molestiae quos, et tenetur nam. Fugit, asperiores blanditiis deserunt explicabo velit vitae error similique? Libero, soluta sint.
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis accusamus praesentium repudiandae esse molestiae quos, et tenetur nam. Fugit, asperiores blanditiis deserunt explicabo velit vitae error similique? Libero, soluta sint.
-      
-    </div>
-  )
+// import { notFound } from "next/navigation";
+
+type PageProps = {
+  params: Promise<{
+    slug: string;
+  }>;
+};
+
+export default async function SingleBlogPage({ params }: PageProps) {
+  const { slug } = await params;
+
+  return <main>{slug}</main>;
 }
-
-export default SingleBlogPage
