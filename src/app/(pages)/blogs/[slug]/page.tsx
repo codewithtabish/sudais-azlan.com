4559f30396ce34f44bc81bf960f88d98fcf6839c
codewithtabish <kashisultan099@gmail.com>
2026-07-26 08:00:45 +0500

    added blog page

---
+++ src/app/(pages)/blogs/[slug]/page.tsx
@@ -0,0 +1,20 @@
+import { getBlogBySlugAction } from "@/actions/(blogs)/get-blog-by-slug";
+import { notFound } from "next/navigation";
+
+type PageProps = {
+  params: Promise<{
+    slug: string;
+  }>;
+};
+
+export default async function SingleBlogPage({ params }: PageProps) {
+  const { slug } = await params;
+
+  const result = await getBlogBySlugAction(slug);
+
+  if (!result.success || !result.data) {
+    notFound();
+  }
+
+  return <main>Blog works</main>;
+}
