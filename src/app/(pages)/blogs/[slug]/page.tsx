d0af565ee4ca7ebf09760042033116017d27efcd
codewithtabish <kashisultan099@gmail.com>
2026-07-26 08:11:15 +0500

    cpmmit blog

--- src/app/(pages)/blogs/[slug]/page.tsx
+++ src/app/(pages)/blogs/[slug]/page.tsx
@@ -1,20 +1,34 @@
-// import { getBlogBySlugAction } from "@/actions/(blogs)/get-blog-by-slug";
-// import { notFound } from "next/navigation";
+// // import { getBlogBySlugAction } from "@/actions/(blogs)/get-blog-by-slug";
+// // import { notFound } from "next/navigation";
 
-type PageProps = {
-  params: Promise<{
-    slug: string;
-  }>;
-};
+// type PageProps = {
+//   params: Promise<{
+//     slug: string;
+//   }>;
+// };
 
-export default async function SingleBlogPage({ params }: PageProps) {
-  const { slug } = await params;
+// export default async function SingleBlogPage({ params }: PageProps) {
+//   const { slug } = await params;
 
-//   const result = await getBlogBySlugAction(slug);
+// //   const result = await getBlogBySlugAction(slug);
 
-//   if (!result.success || !result.data) {
-//     notFound();
-//   }
+// //   if (!result.success || !result.data) {
+// //     notFound();
+// //   }
 
-  return <main>Blog works</main>;
+//   return <main>Blog works</main>;
+// }
+
+import React from 'react'
+
+const SingleBlogPage = () => {
+  return (
+    <div>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis accusamus praesentium repudiandae esse molestiae quos, et tenetur nam. Fugit, asperiores blanditiis deserunt explicabo velit vitae error similique? Libero, soluta sint.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis accusamus praesentium repudiandae esse molestiae quos, et tenetur nam. Fugit, asperiores blanditiis deserunt explicabo velit vitae error similique? Libero, soluta sint.
+      
+    </div>
+  )
 }
+
+export default SingleBlogPage
